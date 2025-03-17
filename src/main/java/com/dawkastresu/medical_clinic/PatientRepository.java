a1cedbbe56405f3431a648878d27089ef0312a18
Dawid <dawidmalczewski3@gmail.com>
2025-03-17 13:39:16 +0100

    PatientController fix i adnotacje

--- src/main/java/com/dawkastresu/medical_clinic/PatientRepository.java
+++ src/main/java/com/dawkastresu/medical_clinic/PatientRepository.java
@@ -1,16 +1,17 @@
 package com.dawkastresu.medical_clinic;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+//Adnotacja Repository mówi, że ta klasa jest beanem odpowiedzialnym za komunikacje z bazą danych, w tym wypadku z Listą pacjentów.
 @Repository
+//ReauiredArgsConstruktor generuje konstruktor który przyjmuje pola finalne
 @RequiredArgsConstructor
 public class PatientRepository {
+    //pusta lista domyślnie jest odczytywana jako bean i obslugiwana przez Spring
     private final List<Patient> patients;
 
     public List<Patient> findAll() {
--- src/main/java/com/dawkastresu/medical_clinic/PatientService.java
+++ src/main/java/com/dawkastresu/medical_clinic/PatientService.java
@@ -1,19 +1,20 @@
 package com.dawkastresu.medical_clinic;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+//ReauiredArgsConstruktor generuje konstruktor który przyjmuje pola finalne
 @RequiredArgsConstructor
+//Adnotacja Service mówi że ta klasa jest beanem zawierajacym logike biznesową aplikacji
 @Service
 public class PatientService {
+    //PatientRepository również musi być beanem aby Spring mógł dostarczyć instancję tej klasy
     private final PatientRepository patientRepository;
 
     public void editPatientByEmail(String email, Patient newPatient) {
         Patient patient = patientRepository.findPatientByEmail(email)
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new IllegalArgumentException("Patient not found"));
 
         patient.setEmail(newPatient.getEmail());
         patient.setPassword(newPatient.getPassword());
@@ -22,8 +23,23 @@ public class PatientService {
         patient.setLastName(newPatient.getLastName());
         patient.setPhoneNumber(newPatient.getPhoneNumber());
         patient.setBirthday(newPatient.getBirthday());
+    }
 
-        patientRepository.removePatientByEmail(email);
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Patient findPatientByName(String email) {
+        return patientRepository.findPatientByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Patient not found"));
+    }
+
+    public void addNewPatient(Patient patient) {
         patientRepository.addPatient(patient);
     }
+
+    public void removePatientByMail(String email) {
+        patientRepository.removePatientByEmail(email);
+    }
+
 }
